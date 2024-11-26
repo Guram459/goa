dee115b2943117c8b1a46cdecd8c3df01a92ab0d
Guram459 <marimarika1982@gmail.com>
2024-11-26 20:00:48 +0400

    commit

---
+++ day24/classwork/classwork24.py
@@ -0,0 +1,12 @@
+#homework 1
+name = "gurami"
+age = 15
+sentence = "my name is {} and I'm {} years old.".format(name, age)
+print(sentence)
+
+#homework 2
+text = "შაურმა არ არის ხინკალზე კაი"
+words = text.split()
+
+result = "😊".join(words)
+print(result)
