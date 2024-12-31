0d4adbc55864fc979338823fd9ae2644bfe7cc33
Guram459 <marimarika1982@gmail.com>
2024-12-31 15:55:30 +0400

    commit

---
+++ day34/classwork/classwork34.py
@@ -0,0 +1,7 @@
+# classwork 1
+favorite_movies = ("tik time", "iron man 2", "Inception", "The Dark Knight", "Pulp Fiction", "Forrest Gump")
+
+first_movie, *remaining_movies = favorite_movies
+
+print("პირველი ფილმი:", first_movie)
+print("დარჩენილი ფილმები:", remaining_movies)
