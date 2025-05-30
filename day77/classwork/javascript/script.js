6eaf6c19f724baedba5e75f74ece6e69cf60eef8
Guram459 <marimarika1982@gmail.com>
2025-05-30 23:08:29 +0400

    commit

---
+++ day77/classwork/javascript/script.js
@@ -0,0 +1,18 @@
+// CLASSWORK 1
+
+let car = ["Mercedes", "BMW", "ford fusion", "ziguli", "ferrari"]
+
+car.splice(1, 3, "hello", "world")
+console.log(car)
+const Carlog = car.slice(2, 3)
+console.log(Carlog)
+
+// CLASSWORK 2
+
+const human = {
+    name: "lado",
+    surname: "Nijaradze",
+    age: "500bc",
+}
+
+console.log(human)
---
+++ day77/homework/javascript/script.js
@@ -0,0 +1,24 @@
+// HOMEWORK 1
+
+let name = ["josh", "nick", "jessica", "taylor", "jamal"]
+
+name.splice(1, 3, "davit", "andria")
+console.log(name)
+const Nameid = name.slice(2, 3)
+console.log(Nameid)
+
+// HOMEWORK 2
+
+let continent = ["africa", "south america", "north america", "asia", "europa", "AUSTRALIA"]
+
+continent.splice(3, 4, "rezi", "lado", "davit")
+console.log(continent)
+const cont = continent.slice(3, 6)
+console.log(cont)
+
+// HOMEWORK 3
+
+let ABC = ['a', 'b', 'c', 'd', 'e']
+
+ABC.splice(2, 3)
+console.log(ABC)
